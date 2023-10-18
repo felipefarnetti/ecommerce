36239a6300281ff9b4419cf7c0427303fb5054b7
Felipe Farnetti <felipefarnetti@MacBook-Air-de-Felipe.local>
2023-10-18 15:20:52 +0200

    all working - less categories NEXT 13.4.2

--- app/utils/categories.ts
+++ app/utils/categories.ts
@@ -1,12 +1,8 @@
 const categories = [
   "Home Improvement",
-  "Outdoor and Garden",
   "Art and Crafts",
-  "Food and Beverages",
   "Jewelry and Watches",
-  "Travel and Luggage",
   "Gifts and Occasions",
-  "Industrial and Scientific",
   "Electronics Accessories",
 ];
 
